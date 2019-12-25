00b19d649712fd5f32e2151d1163752061094b1d
Song-YunMin <48168717+SongYunMin@users.noreply.github.com>
2019-12-25 20:44:54 +0900

    Argorithm

---
+++ BaekJun/2839_설탕배달.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int sugar;
+	int count_3 = 0;
+	int count_5 = 0;
+
+	scanf("%d", &sugar);
+
+	while (sugar >= 3 && sugar != 0) {
+		if ((sugar % 5) >= 3 && sugar != 4) {
+			count_5++;
+			sugar -= 5;
+		}
+		else {
+			count_3++;
+			sugar -= 3;
+		}
+	}
+
+	printf("%d", count_3 + count_5);
+
+	return 0;
+}
