3f41899afd2d72d5eae92eb140fe7559533367de
Song-YunMin <48168717+SongYunMin@users.noreply.github.com>
2019-12-26 10:23:33 +0900

    wqe

--- BaekJun/1712_손익분기점.c
+++ BaekJun/1712_손익분기점.c
@@ -1,24 +1,24 @@
-//#include <stdio.h>
-//
-//int main(void)
-//{
-//	int count = 0 , temp = 0;			
-//	int Static_money;		// 고정 비용
-//	int Dynamic_money;		// 가변 비용
-//	int NoteBook_Price;		// 노트북 비용
-//
-//	scanf("%d %d %d", &Static_money, &Dynamic_money, &NoteBook_Price);
-//	if()
-//	
-//	while (temp <= (Static_money + Dynamic_money))
-//	{
-//		temp += NoteBook_Price;
-//		count++;
-//	}
-//
-//	printf("%d", count);
-//
-//
-//
-//	return 0;
-//}
+#include <stdio.h>
+
+int main(void)
+{
+	int count = 0 , temp = 0;			
+	int Static_money;		// 고정 비용
+	int Dynamic_money;		// 가변 비용
+	int NoteBook_Price;		// 노트북 비용
+
+	scanf("%d %d %d", &Static_money, &Dynamic_money, &NoteBook_Price);
+	
+	while (temp <= (Static_money + Dynamic_money))
+	{
+		//dasdsad
+		temp += NoteBook_Price;
+		count++;
+	}
+
+	printf("%d", count);
+
+
+
+	return 0;
+}
